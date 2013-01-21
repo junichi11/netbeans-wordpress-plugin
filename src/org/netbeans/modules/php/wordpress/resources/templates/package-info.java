6f3a547e838a637e0ce46a7af92905b5e200835e
junichi11 <junichi0206@gmail.com>
2013-01-21 21:35:47 +0900

    Add theme style template

--- src/org/netbeans/modules/php/wordpress/resources/templates/package-info.java
+++ src/org/netbeans/modules/php/wordpress/resources/templates/package-info.java
@@ -19,11 +19,19 @@
             displayName = "#WordPress_Plugin_Readme_DisplayName",
             content = "readme.txt",
             description = "WpPluginReadmeDescription.html",
-            scriptEngine = "freemarker")})
+            scriptEngine = "freemarker"),
+    @TemplateRegistration(
+            folder = "WordPress",
+            iconBase = WordPress.WP_ICON_16,
+            displayName = "#WordPress_Theme_Style_DisplayName",
+            content = "style.css",
+            description = "WpThemeStyleDescription.html",
+            scriptEngine = "freemarker"),})
 @NbBundle.Messages({
     "WordPress_License_Template_DisplayName=GPLv2",
-    "WordPress_Plugin_Template_DisplayName=WordPress Plugin",
-    "WordPress_Plugin_Readme_DisplayName=WordPress Plugin Readme"
+    "WordPress_Plugin_Template_DisplayName=WordPress Plugin File",
+    "WordPress_Plugin_Readme_DisplayName=WordPress Plugin Readme File",
+    "WordPress_Theme_Style_DisplayName=WordPress Theme Style File"
 })
 package org.netbeans.modules.php.wordpress.resources.templates;
 
