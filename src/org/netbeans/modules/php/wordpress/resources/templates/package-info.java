7e4943fcfedb2b68efdf12a06ee5f867eb2ea977
junichi11 <junichi0206@gmail.com>
2014-03-04 07:26:15 +0900

    Add a .htaccess template for permalink #32

--- src/org/netbeans/modules/php/wordpress/resources/templates/package-info.java
+++ src/org/netbeans/modules/php/wordpress/resources/templates/package-info.java
@@ -26,13 +26,20 @@
             displayName = "#WordPress_Theme_Style_DisplayName",
             content = "style.css",
             description = "WpThemeStyleDescription.html",
+            scriptEngine = "freemarker"),
+    @TemplateRegistration(
+            folder = "WordPress",
+            //            iconBase = WordPress.WP_ICON_16,
+            displayName = "#WordPress_Permalink_Htaccess_DisplayName",
+            content = ".htaccess",
+            description = "WpPermalinkHtaccessDescription.html",
             scriptEngine = "freemarker"),})
 @NbBundle.Messages({
     "WordPress_License_Template_DisplayName=GPLv2",
     "WordPress_Plugin_Template_DisplayName=WordPress Plugin File",
     "WordPress_Plugin_Readme_DisplayName=WordPress Plugin Readme File",
-    "WordPress_Theme_Style_DisplayName=WordPress Theme Style File"
-})
+    "WordPress_Theme_Style_DisplayName=WordPress Theme Style File",
+    "WordPress_Permalink_Htaccess_DisplayName=WordPress .htaccess file for permalink"})
 package org.netbeans.modules.php.wordpress.resources.templates;
 
 import org.netbeans.api.templates.TemplateRegistration;
