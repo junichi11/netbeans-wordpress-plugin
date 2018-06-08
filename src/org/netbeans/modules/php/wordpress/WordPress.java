15796b0ae90fc6249dfce4fa837498ffe3d106b8
junichi11 <junichi0206@gmail.com>
2018-06-08 11:18:28 +0900

    Add @StaticResource

--- src/org/netbeans/modules/php/wordpress/WordPress.java
+++ src/org/netbeans/modules/php/wordpress/WordPress.java
@@ -41,13 +41,17 @@
  */
 package org.netbeans.modules.php.wordpress;
 
+import org.netbeans.api.annotations.common.StaticResource;
+
 /**
  *
  * @author junichi11
  */
 public final class WordPress {
 
+    @StaticResource
     public static final String WP_ICON_8 = "org/netbeans/modules/php/wordpress/resources/wordpress_icon_8.png"; // NOI18N
+    @StaticResource
     public static final String WP_ICON_16 = "org/netbeans/modules/php/wordpress/resources/wordpress_icon_16.png"; // NOI18N
 
     private WordPress() {
